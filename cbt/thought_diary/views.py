903a783de41dbf14d6069b6c14aa55233d90b6c3
Ben Earle <bearle2009@gmail.com>
2011-07-23 09:34:09 -0400

    started working on challenge thought

--- cbt/thought_diary/views.py
+++ cbt/thought_diary/views.py
@@ -77,21 +77,33 @@ def logoutView(request):
 	return redirect("/")
 
 def thoughtAction(request):
-	pass
-	
-def deleteThought(request):
 	if request.method=="POST":
 		if request.user.is_authenticated():
 			id=request.POST['id']
 			action=request.POST['action']
-			print id 
-			print action
 			thought=Thought.objects.get(pk=id)
 			if action=="delete":
 				thought.delete()
 				thoughts=Thought.objects.all()
 				return render(request, "thought_list.html", {'thoughts':thoughts})
-	
+			
+			elif action=="add":
+				form=ThoughtForm(request.POST)
+				moodForm=MoodForm(request.POST)
+				if form.is_valid():
+					temp=form.save(commit=False)
+					temp.user=request.user
+					temp.save()
+				else:
+					print 'invalid'
+				if moodForm.is_valid():
+					temp=moodForm.save(commit=False)
+					temp.user=request.user
+					temp.save()
+					return redirect("/thought")
+				else:
+					print 'invalid'		
+
 def thoughtView(request):
 	if request.method=="POST":
 		form=ThoughtForm(request.POST)
@@ -121,6 +133,11 @@ def thoughtView(request):
 		c={'form':form, 'moodForm':moodForm, 'thoughts':thoughts, 'logged_in':logged_in, 'username':username}
 		return render(request, "thought.html", c)
 
+def getThoughts(request):
+	thoughts=Thought.objects.filter(user=request.user).order_by('datetime')
+	c={'thought':thoughts}
+	return render(request, "thought_list.html", c)
+	
 def errorView(request):
 	logged_in=False
 	username=""
