bc5c28c01945753d1e9a3c83e1c5c5e0698a5db9
Ben Earle <bearle2009@gmail.com>
2011-10-03 22:57:45 -0400

    getting distortions working

--- cbt/script.sh
+++ cbt/script.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = "challenges" ]; then
 #python manage.py dumpscript thought_diary.distortion thought_diary.challengequestion thought_diary.challengequestion_distortion --indent 4 > thought_diary/fixtures/challenge_and_distortion.json
-python manage.py dumpdata thought_diary.distortion thought_diary.challengequestion thought_diary.challengequestion_distortion --indent 4 > thought_diary/fixtures/challenge_and_distortion.json
+python manage.py dumpdata thought_diary.distortion thought_diary.challengequestion --indent 4 > thought_diary/fixtures/challenge_and_distortion.json
 
 elif [ "$1" = "test" ]; then
 python manage.py test --verbosity 2 thought_diary
@@ -11,6 +11,8 @@ python manage.py test --verbosity 2 thought_diary
 elif [ "$1" = "load" ]; then
 python manage.py loaddata thought_diary/fixtures/original_data.json
 
+elif [ "$1" = "load_distortions" ]; then
+python manage.py loaddata thought_diary/fixtures/original_challenge_and_distortion.json
 
 elif [ "$1" = "all" ]; then
 python manage.py dumpdata thought_diary --indent 4 > thought_diary/fixtures/original_data.json
